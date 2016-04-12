93b674191515f722a32c0a25ac7c89d6fe79a9f9
Vincent <vincent.henrion@hetic.net>
2016-04-12 09:32:03 +0200

    Modif du sql

--- sql/dblogin.sql
+++ sql/dblogin.sql
@@ -1,30 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jan 07, 2016 at 03:05 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `dblogin`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 CREATE DATABASE `thegamedb`;
 CREATE TABLE IF NOT EXISTS `thegamedb`.`users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -34,6 +11,3 @@ CREATE TABLE IF NOT EXISTS `thegamedb`.`users` (
   `joining_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
